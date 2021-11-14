d9b0f875ea67f8b9df30868c461f3a1fe5743153
Sebastián Henao <sebastianhenaoloaiza@gmail.com>
2021-11-14 00:54:35 -0500

    :construction: Metodo get iniciado

--- api.rb
+++ api.rb
@@ -1,6 +1,21 @@
+require 'dotenv'
+require 'pg'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
-get '/' do
-  'Primer Saludo'
+Dotenv.load('.env')
+
+conn = PG.connect(:dbname => ENV['DB_NAME'], :host => ENV['DB_HOST'], :port => ENV['DB_PORT'], :user => ENV['DB_USER'], :password => ENV['DB_PASS'])
+
+p conn.server_version
+
+before do
+  content_type :json
+end
+
+# TODO: Solucionar esta "chapuza"
+get '/temperature' do
+  conn.exec("SELECT * FROM temperature").each{|row| @res = row}
+  @res.to_json
 end
