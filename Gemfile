efb88a2974cefafb3a8f2ea1897b2e5ff043ed20
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2021-11-22 19:37:21 -0500

    Heroku deploy test 1

--- Gemfile
+++ Gemfile
@@ -6,5 +6,4 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'sinatra', '~> 2.1'
 gem 'sinatra-contrib', '~> 2.1'
-gem 'dotenv', '~> 2.7', '>= 2.7.6'
 gem 'pg', '~> 1.2', '>= 1.2.3'
--- api.rb
+++ api.rb
@@ -1,10 +1,8 @@
-require 'dotenv'
 require 'pg'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-Dotenv.load('.env')
 
 conn = PG.connect(:dbname => ENV['DB_NAME'], :host => ENV['DB_HOST'], :port => ENV['DB_PORT'], :user => ENV['DB_USER'], :password => ENV['DB_PASS'])
 
