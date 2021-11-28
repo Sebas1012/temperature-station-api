d61303a8f0baa8e6aa214c844d160aa47ddb703e
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2021-11-27 21:08:50 -0500

    :beers: Update

--- api.rb
+++ api.rb
@@ -1,8 +1,10 @@
 require 'pg'
 require 'sinatra'
-require 'sinatra/reloader'
+# require 'sinatra/reloader'
 require 'json'
+# require 'dotenv'
 
+# Dotenv.load('.env')
 
 set :bind, '0.0.0.0'
 set :server, :puma
@@ -17,6 +19,23 @@ end
 
 # TODO: Solucionar esta "chapuza"
 get '/temperature' do
-  conn.exec("SELECT * FROM temperature").each{|row| @res = row}
+  res = conn.exec("SELECT * FROM temperature")
+
+  data = []
+
+  res.each{|row| data.push(row)}
+  data.to_json
+end
+
+get '/temperature/:id' do
+  conn.exec("SELECT * FROM temperature WHERE id = #{params[:id]}").each{|row| @res = row}
   @res.to_json
 end
+
+post '/temperature' do
+  data = JSON.parse request.body.read
+
+  conn.exec("INSERT INTO temperature(temperature, humidity, full_date, full_hour) 
+            VALUES(#{data['temperature'].to_f}, #{data['humidity'].to_f}, '#{data['full_date']}', '#{data['full_hour']}')")
+end
+
